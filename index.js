f2fcc227c97d7581fc04f0360bc93e2e278fc35c
unknown <tomalhossen78@gmail.com>
2025-12-16 04:22:57 +0600

    done

--- index.js
+++ index.js
@@ -901,6 +901,4 @@ app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
-// app.listen(port, () => {
-//   console.log(`Example app listening on port ${port}`);
-// });
+module.exports = app;
